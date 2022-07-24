2a8d0e8456e9aa2a4c45ec964c16eb5310061e8a
Vitaliy Zhyryk <80642298+WindyBoss@users.noreply.github.com>
2022-07-24 18:17:24 +0200

    Updates

---
+++ static/js/833.6421544d.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[833],{2994:function(n,e,i){i.r(e),i.d(e,{Review:function(){return a},ReviewsContainer:function(){return s}});var r,t,o=i(168),u=i(7939),a=u.ZP.li(r||(r=(0,o.Z)(["\n  border: 1px solid black;\n  padding: 15px;\n  border-radius: 5px;\n"]))),s=u.ZP.ul(t||(t=(0,o.Z)(["\n  padding: 0px;\n"])))}}]);
+//# sourceMappingURL=833.6421544d.chunk.js.map
